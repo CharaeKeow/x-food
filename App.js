428028d0e2c3cc717d0549ed71baba89f97a27f4
CharaeKeow <charaekeow11@gmail.com>
2021-04-26 01:17:49 +0800

    init commit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const clicked = () => {
+    alert("Thank you")
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button onPress={clicked} title="Sign Up" />
       <StatusBar style="auto" />
     </View>
   );
